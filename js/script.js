539afdf2ea2e61e55d086b5bacba4ed538ad9643
Pasha_JS <info@snab.kiev.ua>
2022-11-19 16:34:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+	"use strict";
+//alert('Hello');
+//const result = confirm("Are you there");
+//console.log(result);
+//const answer = prompt("kto ti", "");
+//console.log(answer);
+//const answers = [];
+
+//answers[0] = prompt('why you?', '');
+//answers[1] = prompt('why name?', '');
+//answers[2] = prompt('why you old?', '');
+//
+//document.write(answers);
+//console.log(typeof(answers));
